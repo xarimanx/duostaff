56fe257adca4d46cb90350956eb74fe079629a96
Evgeniy Maslov <eum@anahoret.com>
2013-08-30 13:43:19 +0300

    add welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Duostaff::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
@@ -53,4 +53,5 @@ Duostaff::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root 'welcome#main'
 end
